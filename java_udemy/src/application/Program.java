16f8c388ee76ab70cb3413aec962539f2b39fb74
vickats <32991496+vickats@users.noreply.github.com>
2024-06-12 17:22:15 -0300

    - Métodos abstratos
    + Exercicios

--- java_udemy/src/application/Program.java
+++ java_udemy/src/application/Program.java
@@ -5,9 +5,9 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
-import entities.Account;
-import entities.BusinessAccount;
-import entities.SavingsAccount;
+import entities.Company;
+import entities.Individual;
+import entities.TaxPayer;
 
 public class Program {
 
@@ -2144,31 +2144,134 @@ public class Program {
 //		Account acc2 = new SavingsAccount(1002, "Maria", 1100.0, 0.01);
 //		Account acc3 = new BusinessAccount(1003, "João", 1100.0, 500.0);
 		
-		//Exemplo de aplicação: totalizar o saldo de todas as contas e acrescentar 10.00 em todas as contas
+		//Exercicio: totalizar o saldo de todas as contas e acrescentar 10.00 em todas as contas (https://github.com/acenelio/inheritance6-java)
 
 		//Account - tipo genérico
-		List<Account> list = new ArrayList<>();
-		
-		list.add(new SavingsAccount(1001, "João", 500.0, 0.01));
-		list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
-		list.add(new SavingsAccount(1003, "Bob", 300.0, 0.01));
-		list.add(new BusinessAccount(1004, "Ana", 500.0, 500.0));
+//		List<Account> list = new ArrayList<>();
+//		
+//		list.add(new SavingsAccount(1001, "João", 500.0, 0.01));
+//		list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+//		list.add(new SavingsAccount(1003, "Bob", 300.0, 0.01));
+//		list.add(new BusinessAccount(1004, "Ana", 500.0, 500.0));
 		
 		//soma do saldo de todas as contas
-		double sum = 0;		
-		for(Account acc : list) {
-			sum += acc.getBalance();
-		}		
-		System.out.printf("Total balance: %.2f%n", sum);
+//		double sum = 0;		
+//		for(Account acc : list) {
+//			sum += acc.getBalance();
+//		}		
+//		System.out.printf("Total balance: %.2f%n", sum);
 		
 		//saldo de todas as contas acrescido de 10.00 e seus respectivos numeros
-		for(Account acc : list) {
-			acc.deposit(10.0);
-		}		
-		for(Account acc : list) {
-			System.out.printf("Updated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+//		for(Account acc : list) {
+//			acc.deposit(10.0);
+//		}		
+//		for(Account acc : list) {
+//			System.out.printf("Updated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+//		}
+		
+		//Métodos abstratos
+		
+		/* São métodos que não possuem implementação. Métodos precisam ser abstratos quando a cl asse é genérica demais
+		 * para conter sua implementação. Se uma classe possuir pelo menos um método abstrato, então esta classe também
+		 * é abstrata. No UML, sua notação é em itálico.
+		 */
+		
+		//Exercicio resolvido - (https://github.com/acenelio/inheritance7-java)
+		
+		/* Fazer um programa para ler os dados de N figuras (N fornecido pelo usuário), e 
+		 * depois mostrar as áreas destas figuras na mesma ordem em que foram digitadas.
+		 */
+		
+//		List<Shape> list = new ArrayList<>();	
+//		
+//		System.out.print("Enter the number of shapes: ");
+//		int n = sc.nextInt();
+//		
+//		for(int i = 1; i <= n; i++) {
+//			System.out.println("Shape #" + i + " data:");
+//			System.out.print("Rectangle or Circle (r/c)? ");
+//			char ch = sc.next().charAt(0);	
+//			System.out.print("Color (BLACK/BLUE/RED): ");
+//			//o que for digitado aqui será convertido para objeto do tipo enumerado Color
+//			Color color = Color.valueOf(sc.next());
+//			
+//			if(ch == 'r') {
+//				System.out.print("Width: ");
+//				double width =  sc.nextDouble();
+//				System.out.print("Height: ");
+//				double height = sc.nextDouble();
+//				list.add(new Rectangle2(color, width, height));
+//			}else {
+//				System.out.print("Radius: ");
+//				double radius = sc.nextDouble();
+//				list.add(new Circle(color, radius));				
+//			}
+//			
+//		}		
+//		
+//		System.out.println();
+//		System.out.println("SHAPES AREAS:");
+//		for(Shape sh : list) {
+//			System.out.println(String.format("%.2f", sh.area()));
+//		}
+		
+		//Exercício de fixação (https://github.com/acenelio/inheritance8-java)
+		
+		/* Fazer um programa para ler os dados de N contribuintes (N fornecido pelo usuário), os quais
+		 * podem ser pessoa física ou pessoa jurídica, e depois mostrar o valor do imposto pago por cada um,
+		 * bem como o total de imposto arrecadado.
+		 * Os dados de pessoa física são: nome, renda anual e gastos com saúde. Os dados de pessoa jurídica
+		 * são nome, renda anual e número de funcionários. As regras para cálculo de imposto são as
+		 * seguintes:
+		 * Pessoa física: pessoas cuja renda foi abaixo de 20000.00 pagam 15% de imposto. Pessoas com
+		 * renda de 20000.00 em diante pagam 25% de imposto. Se a pessoa teve gastos com saúde, 50%
+		 * destes gastos são abatidos no imposto.
+		 * Exemplo: uma pessoa cuja renda foi 50000.00 e teve 2000.00 em gastos com saúde, o imposto
+		 * fica: (50000 * 25%) - (2000 * 50%) = 11500.00
+		 * Pessoa jurídica: pessoas jurídicas pagam 16% de imposto. Porém, se a empresa possuir mais de 10
+		 * funcionários, ela paga 14% de imposto.
+		 * Exemplo: uma empresa cuja renda foi 400000.00 e possui 25 funcionários, o imposto fica:
+		 * 400000 * 14% = 56000.00
+		 */
+		
+		List<TaxPayer> list = new ArrayList<>();		
+		
+		System.out.print("Enter the number of tax payers: ");
+		int n = sc.nextInt();
+		
+		for(int i = 1; i <= n; i++) {
+			System.out.println("Tax payer #" + i + " data:");
+			System.out.print("Individual or company (i/c)? ");
+			char ch = sc.next().charAt(0);
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Anual income: ");
+			Double anualIncome = sc.nextDouble();
+			if(ch == 'c') {				
+				System.out.print("Number of employees: ");
+				int numberOfEmployees = sc.nextInt();
+				list.add(new Company(name, anualIncome, numberOfEmployees));
+			}else {
+				System.out.print("Health expenditures: ");
+				Double healthExpenditures = sc.nextDouble();
+				list.add(new Individual(name, anualIncome, healthExpenditures));
+			}
 		}
-			
+		
+		System.out.println();		
+		System.out.println("TAXES PAID:");
+		
+		double sum = 0.0;
+		
+		for(TaxPayer taxPayer : list) {
+			System.out.println(taxPayer.getName() + ": $ " + String.format("%.2f", taxPayer.tax()));
+			sum += taxPayer.tax();
+		}
+		
+		System.out.printf("%nTOTAL TAXES: $ %.2f", sum);
+
+		
 		sc.close();	
 		
 	}
