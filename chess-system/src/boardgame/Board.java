b26d20713b0229171b26ecae7f5db6d2039f56da
vickats <32991496+vickats@users.noreply.github.com>
2024-06-20 15:23:05 -0300

    Moving pieces

--- chess-system/src/boardgame/Board.java
+++ chess-system/src/boardgame/Board.java
@@ -42,6 +42,20 @@ public class Board {
 			throw new BoardException("Position not on the board");
 		}
 		return pieces[position.getRow()][position.getColumn()];
+	}	
+	
+	//remover peças
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
 	}
 	
 	//atribui a peça na posição informada, não estando mais nula
