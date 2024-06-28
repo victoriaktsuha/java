91cdc0c0827a704507c93df8bc9c470d1e216a82
vickats <32991496+vickats@users.noreply.github.com>
2024-06-28 09:49:40 -0300

    Check logic

--- chess-system/src/chess/ChessPosition.java
+++ chess-system/src/chess/ChessPosition.java
@@ -27,8 +27,8 @@ public class ChessPosition {
 		return new Position(8 - row, column - 'a');
 	}
 	
-	protected static ChessPosition fromPostion(Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getColumn());
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char)('a' + position.getColumn()), 8 - position.getRow());
 	}
 
 	@Override
