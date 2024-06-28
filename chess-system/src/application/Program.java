892919fb692f43dec9d7de72e39e3f71bebd39af
vickats <32991496+vickats@users.noreply.github.com>
2024-06-28 10:20:21 -0300

    Checkmate logic

--- chess-system/src/application/Program.java
+++ chess-system/src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while(true) {
+		while(!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				//imprime o tabuleiro com as peças
@@ -52,6 +52,7 @@ public class Program {
 				sc.nextLine();
 			}
 		}
-
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
