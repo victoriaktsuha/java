2a9abe68e972878fd1a86cac5a2f7c0b94d1c0ff
vickats <32991496+vickats@users.noreply.github.com>
2024-06-28 10:44:48 -0300

    Piece move count

--- chess-system/src/chess/ChessMatch.java
+++ chess-system/src/chess/ChessMatch.java
@@ -91,7 +91,8 @@ public class ChessMatch {
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece) board.removePiece(source);
+		p.increseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		
@@ -104,7 +105,8 @@ public class ChessMatch {
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreseMoveCount();
 		board.placePiece(p, source);
 		
 		if(capturedPiece != null) {
