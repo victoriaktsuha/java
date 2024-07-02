8cc2a7e0c30f2bfc734195ce7fa65e2ea1b3013b
vickats <32991496+vickats@users.noreply.github.com>
2024-07-02 10:43:47 -0300

    217. Bloco try-with-resources

--- java_udemy/src/application/FilesAndScanner.java
+++ java_udemy/src/application/FilesAndScanner.java
@@ -12,22 +12,22 @@ public class FilesAndScanner {
 		
 		//215. Lendo arquivos de etxtos com as classes File e Scanner
 		
-		File file = new File("c:\\tmp\\in.txt");		
-		Scanner sc = null;
-		try {
-			sc = new Scanner(file);
-			while (sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
-			}
-		}
-		catch(IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-		finally {
-			if(sc != null) {				
-				sc.close();
-			}
-		}
+//		File file = new File("c:\\tmp\\in.txt");		
+//		Scanner sc = null;
+//		try {
+//			sc = new Scanner(file);
+//			while (sc.hasNextLine()) {
+//				System.out.println(sc.nextLine());
+//			}
+//		}
+//		catch(IOException e) {
+//			System.out.println("Error: " + e.getMessage());
+//		}
+//		finally {
+//			if(sc != null) {				
+//				sc.close();
+//			}
+//		}
 		
 		//216. FileReader e BufferedReader (classes)
 		
@@ -39,14 +39,50 @@ public class FilesAndScanner {
 		
 		
 		//Implementação manual
-		String path = "c:\\tmp\\in.txt";
-		FileReader fr = null;
-		BufferedReader br = null;
+//		String path = "c:\\tmp\\in.txt";
+//		FileReader fr = null;
+//		BufferedReader br = null;
+//		
+//		try {
+//			fr = new FileReader(path);
+//			br = new BufferedReader(fr);
+//			
+//			String line = br.readLine();
+//			
+//			while (line != null){
+//				System.out.println(line);
+//				line = br.readLine();
+//			}
+//		}
+//		catch(IOException e) {
+//			System.out.println("Error: " + e.getMessage());
+//		}
+//		finally {			
+//			try {
+//				
+//				if(br != null) {				
+//					br.close();
+//				}
+//				if(fr != null) {				
+//					fr.close();
+//				}
+//			}
+//			catch(IOException e) {
+//				e.printStackTrace();
+//			}
+//		}		
 		
-		try {
-			fr = new FileReader(path);
-			br = new BufferedReader(fr);
-			
+		//217. Bloco try-with-resources
+		
+		/*
+		 * É um bloco try que declara um ou mais recursos, e garante que esses recursos serão
+		 * fechados ao final do bloco - Disponível no Java 7 em diante
+		 */
+		
+		String path = "c:\\tmp\\in.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
 			String line = br.readLine();
 			
 			while (line != null){
@@ -57,21 +93,8 @@ public class FilesAndScanner {
 		catch(IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
-		finally {			
-			try {
-				
-				if(br != null) {				
-					br.close();
-				}
-				if(fr != null) {				
-					fr.close();
-				}
-			}
-			catch(IOException e) {
-				e.printStackTrace();
-			}
-		}		
 		
+
 	}
 	
 }
