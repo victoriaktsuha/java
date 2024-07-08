94a470e9cc1c1a9af8847ce415734527dfca7528
vickats <32991496+vickats@users.noreply.github.com>
2024-07-08 12:22:29 -0300

    241. Tipos curinga (wildcard types)

--- java_udemy/src/application/Generics.java
+++ java_udemy/src/application/Generics.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import entities.ProductGenerics;
@@ -12,6 +13,7 @@ import services.CalculationService;
 public class Generics {
 
 	public static void main(String[] args) {
+		/* START MAIN() */
 
 		// 239. Introdução a Generics
 
@@ -107,36 +109,94 @@ public class Generics {
 		 * um conjunto de produtos a partir de um arquivo, conforme exemplo, e depois
 		 * mostre o mais caro deles.
 		 * 
-		 * Criar um serviço de cálculo:
-		 * -----------------------------
-		 * CalculationService
-		 * -----------------------------
-		 * -----------------------------
-		 * + max<T>(list: List<T>): T
+		 * Criar um serviço de cálculo: ----------------------------- CalculationService
+		 * ----------------------------- ----------------------------- + max<T>(list:
+		 * List<T>): T
 		 * 
 		 */
+
+//		List<ProductGenerics> list = new ArrayList<>();
+//		
+//		String path = "C:\\tmp\\in.txt";
+//		
+//		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+//			String line = br.readLine();
+//			while(line != null) {
+//				//vetor para armazenar conteúdo antes e depois da vírgula, separadamente
+//				String[] fields = line.split(",");
+//				//converter string da fields[1] em double
+//				list.add(new ProductGenerics(fields[0], Double.parseDouble(fields[1])));
+//				line = br.readLine();
+//			}
+//			
+//			ProductGenerics x = CalculationService.max(list);
+//			System.out.println("Most expensive:");
+//			System.out.println(x);
+//		}
+//		catch(IOException e) {
+//			System.out.println("Error: " + e.getMessage());
+//		}
+
+		// 241. Tipos curinga (wildcard types)
+
+		/*
+		 * Generics são invariantes
+		 * 
+		 * List<Object> não é o supertipo de qualquer tipo de lista:
+		 * 
+		 * List<Object> myObjs = new ArrayList<Object>(); List<Integer> myNumbers = new
+		 * ArrayList<Integer>(); myObjs = myNumbers; // erro de compilação
+		 * 
+		 * O supertipo de qualquer tipo de lista é List<?>. Este é um tipo curinga:
+		 * 
+		 * List<?> myObjs = new ArrayList<Object>(); List<Integer> myNumbers = new
+		 * ArrayList<Integer>(); myObjs = myNumbers;
+		 * 
+		 */
+
+		// Aqui está correto - integer é um subtipo de object, ocorre aqui upcasting
+//		Object obj;
+//		Integer x = 10;
+//		obj = x;
+
+		// ! integer é um subtipo de object, MAS List<Integer> NÃO É um subtipo de
+		// List<Object>
+		// Aqui está incorreto, pois lista de integer não é lista de object
+//		List<Object> myObjs = new ArrayList<Object>(); 
+//		List<Integer> myNumbers = new ArrayList<Integer>(); 
+//		myObjs = myNumbers; // erro de compilação - não é possivel fazer um upcasting
+
+		// <?> supertipo de qualquer tipo de lista
+//		List<?> myObjs = new ArrayList<Object>();
+//		List<Integer> myNumbers = new ArrayList<Integer>();
+//		myObjs = myNumbers;
+
+		/*
+		 * Com tipos curinga podemos fazer métodos que recebem um genérico de "qualquer tipo":
+		 */
+
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
+
+		List<String> myStrs = Arrays.asList("Maria", "Bob");
+		printList(myStrs);
+
+		/*
+		 * Porém não é possível adicionar dados a uma coleção de tipo curinga:
+		 */
 		
-		//
-		List<ProductGenerics> list = new ArrayList<>();
+		List<?> list = new ArrayList<Integer>();
+		list.add(3); // erro de compilação
 		
-		String path = "C:\\tmp\\in.txt";
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))){
-			String line = br.readLine();
-			while(line != null) {
-				//vetor para armazenar conteúdo antes e depois da vírgula, separadamente
-				String[] fields = line.split(",");
-				//converter string da fields[1] em double
-				list.add(new ProductGenerics(fields[0], Double.parseDouble(fields[1])));
-				line = br.readLine();
-			}
-			
-			ProductGenerics x = CalculationService.max(list);
-			System.out.println("Most expensive:");
-			System.out.println(x);
-		}
-		catch(IOException e) {
-			System.out.println("Error: " + e.getMessage());
+		//O compilador não sabe qual é o tipo específico do qual a lista foi instanciada.
+	}
+
+	/* END MAIN() */
+
+	// método para aula 241. Tipos curinga - imprime uma lista de qualquer tipo
+	public static void printList(List<?> list) {
+		for (Object obj : list) {
+			System.out.println(obj);
 		}
 	}
 }
