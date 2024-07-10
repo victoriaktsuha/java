99e19006a1698ba3ce4bf8878e0a0a61bc2077cb
vickats <32991496+vickats@users.noreply.github.com>
2024-07-10 15:05:06 -0300

    243. hashCode e equals

--- java_udemy/src/application/Generics.java
+++ java_udemy/src/application/Generics.java
@@ -1,12 +1,6 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import entities.CircleGenerics;
-import entities.RectangleGenerics;
-import entities.ShapeGenerics;
+import entities.ClientGenerics;
 
 public class Generics {
 
@@ -135,7 +129,7 @@ public class Generics {
 //			System.out.println("Error: " + e.getMessage());
 //		}
 
-		// 241. Tipos curinga (wildcard types) 
+		// 241. Tipos curinga (wildcard types)
 		/*
 		 * Generics são invariantes
 		 * 
@@ -169,7 +163,8 @@ public class Generics {
 //		myObjs = myNumbers;
 
 		/*
-		 * Com tipos curinga podemos fazer métodos que recebem um genérico de "qualquer tipo":
+		 * Com tipos curinga podemos fazer métodos que recebem um genérico de
+		 * "qualquer tipo":
 		 */
 
 //		List<Integer> myInts = Arrays.asList(5, 2, 10);
@@ -181,22 +176,23 @@ public class Generics {
 		/*
 		 * Porém não é possível adicionar dados a uma coleção de tipo curinga:
 		 */
-		
+
 //		List<?> list = new ArrayList<Integer>();
 //		list.add(3); // erro de compilação
-		
-		//O compilador não sabe qual é o tipo específico do qual a lista foi instanciada.
-		
-		
-		
-		//242. Curingas delimitados (bounded wildcards) (https://github.com/acenelio/generics4-java)
-		
-		/* Vamos fazer um  método para retornar a soma das áreas de uma lista de figuras
+
+		// O compilador não sabe qual é o tipo específico do qual a lista foi
+		// instanciada.
+
+		// 242. Curingas delimitados (bounded wildcards)
+		// (https://github.com/acenelio/generics4-java)
+
+		/*
+		 * Vamos fazer um método para retornar a soma das áreas de uma lista de figuras
 		 * 
 		 */
-		
-		//Problema 1 - soluções impróprias
-		
+
+		// Problema 1 - soluções impróprias
+
 //		List<ShapeGenerics> myShapes = new ArrayList<>();
 //		myShapes.add(new RectangleGenerics(3.0, 2.0));
 //		myShapes.add(new CircleGenerics(2.0));
@@ -206,13 +202,11 @@ public class Generics {
 //		myCircles.add(new CircleGenerics(3.0));
 //		
 //		System.out.println("Total area: " + totalArea(myCircles));
-		
-		
-		/* Princípio get/put - covariância
-		 * get - OK
-		 * put (add) - ERROR			
+
+		/*
+		 * Princípio get/put - covariância get - OK put (add) - ERROR
 		 */
-		
+
 //		List<Integer> intList = new ArrayList<Integer>();
 //		intList.add(10);
 //		intList.add(5);
@@ -223,13 +217,11 @@ public class Generics {
 //		Number x = list.get(0);
 //		
 //		list.add(20); // erro de compilacao - o compilador não permite pois não sabe qual tipo especifico será adicionado, já que 'Number' é um tipo mais generico, e pode ter qualquer subtipo como int, double, float, etc
-		
-		
-		/* Princípio get/put - contravariância
-		 * get - ERROR
-		 * put (add) - OK			
+
+		/*
+		 * Princípio get/put - contravariância get - ERROR put (add) - OK
 		 */
-		
+
 //		List<Object> myObjs = new ArrayList<Object>();
 //		myObjs.add("Maria");
 //		myObjs.add("Alex");
@@ -241,22 +233,87 @@ public class Generics {
 //		myNums.add(3.14);
 //		
 //		Number y = myNums.get(0); // erro de compilacao - não é possivel acessar um elemento da lista e tentar atribui-lo a uma variável do tipo Number, pq o tipo da lista pode ser um tipo que seja um supertipo de Number. Adicionar em um variavel do tipo Object seria mais aceito, pois Object é a superclass de Number e último nível
-		
-		
-		//Problema 2 - princípio get/put
-		
-		/* Vamos fazer um método que copia os elementos de uma lista para uma outra lista que pode ser 
-		 * mais genérica que a primeira. 
+
+		// Problema 2 - princípio get/put
+
+		/*
+		 * Vamos fazer um método que copia os elementos de uma lista para uma outra
+		 * lista que pode ser mais genérica que a primeira.
+		 */
+
+//		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+//		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+//		List<Object> myObjs = new ArrayList<Object>();
+//		//copia de myInts/myDoubles para myObjs
+//		copy(myInts, myObjs);
+//		printList(myObjs);
+//		copy(myDoubles, myObjs);
+//		printList(myObjs);
+
+		// 243. hashCode e equals
+
+		/*
+		 * São operações da classe Object utilizadas para comparar se um objeto é igual
+		 * a outro
+		 * 
+		 * • equals: lento, resposta 100% • hashCode: rápido, porém resposta positiva
+		 * não é 100%
+		 * 
+		 * • Tipos comuns (String, Date, Integer, Double, etc.) já possuem implementação
+		 * para essas operações. Classes personalizadas precisam sobrepô-las.
+		 * 
+		 */
+
+		/*
+		 * Equals
+		 * 
+		 * Método que compara se o objeto é igual a outro, retornando true ou false.
+		 */
+
+//		String a = "Maria";
+//		String b = "Alex";
+//
+//		System.out.println(a.equals(b));
+
+		/*
+		 * HashCode 
+		 * 
+		 * Método que retorna um número inteiro representando um código gerado a partir das informações do objeto
+		 * Colisão: Pode acontecer de gerar o mesmo hashCode para obj diferentes, mas nunca hasCode diferente 
+		 * para o mesmo obj
+		 * 
+		 * Regra de ouro:
+		 * - Se o hashCode de dois objetos for diferente, então os dois objetos são diferentes
+		 * - Se o código de dois objetos for igual, muito provavelmente os objetos são iguais (pode haver colisão)
 		 */
 		
-		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
-		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
-		List<Object> myObjs = new ArrayList<Object>();
-		//copia de myInts/myDoubles para myObjs
-		copy(myInts, myObjs);
-		printList(myObjs);
-		copy(myDoubles, myObjs);
-		printList(myObjs);
+//		String c = "Maria";
+//		String d = "Alex";
+//		
+//		System.out.println(a.hashCode());
+//		System.out.println(b.hashCode());
+		
+		ClientGenerics c1 = new ClientGenerics("Maria", "maria@gmail.com");
+		ClientGenerics c2 = new ClientGenerics("Alex", "alex@gmail.com");
+		ClientGenerics c3 = new ClientGenerics("Maria", "alex@gmail.com");
+		ClientGenerics c4 = new ClientGenerics("Maria", "maria@gmail.com");
+		
+		String s1 = "Test";
+		String s2 = "Test";
+		
+		System.out.println(c1.hashCode());
+		System.out.println(c4.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c3.hashCode());
+		System.out.println(c1.equals(c4)); //true - compara o conteúdo
+		
+		System.out.println(c1 == c4); //false pois a referencia de memoria são 2 objetos diferentes
+		 
+		System.out.println(s1 == s2); /* true - pois apesar de ser objetos diferentes, 
+		a expressão literal/string é tratada de outra forma. 
+		Diferente de String s1 = new String("Test"), onde é forçada a criação 
+		de um novo objeto*/
+	
 	}
 
 	/* END MAIN() */
@@ -267,9 +324,10 @@ public class Generics {
 //			System.out.println(obj);
 //		}
 //	}
-	
+
 	// método para aula 242. Curingas delimitados - problema 1
-	//outdated - essa solução de método só funciona para uma lista<ShapeGenerics>; Caso a lista seja de um subtipo (que extende) de ShapeGeneics, não funciona
+	// outdated - essa solução de método só funciona para uma lista<ShapeGenerics>;
+	// Caso a lista seja de um subtipo (que extende) de ShapeGeneics, não funciona
 //	public static double totalArea(List<ShapeGenerics> list) {
 //		double sum = 0.0;
 //		for(ShapeGenerics s : list) {
@@ -277,9 +335,11 @@ public class Generics {
 //		}
 //		return sum;
 //	}
-	
+
 	// método para aula 242. Curingas delimitados - problema 1
-	//refatorado - essa solução de método funciona caso a lista seja de tipo ShapeGenerics ou de um subtipo (que extende) de ShapeGeneics, mas ainda não é possivel adicionar elementos na lista
+	// refatorado - essa solução de método funciona caso a lista seja de tipo
+	// ShapeGenerics ou de um subtipo (que extende) de ShapeGeneics, mas ainda não é
+	// possivel adicionar elementos na lista
 //		public static double totalArea(List<? extends ShapeGenerics> list) {
 //			double sum = 0.0;
 //			for(ShapeGenerics s : list) {
@@ -287,18 +347,18 @@ public class Generics {
 //			}
 //			return sum;
 //		}
-	
+
 	// método para aula 242. Curingas delimitados - problema 2
-	public static void copy(List<? extends Number> source, List<? super Number> target){
-		for(Number n : source) {
-			target.add(n);
-		}
-	}
-	
-	public static void printList(List<?> list) {
-		for(Object obj : list) {
-			System.out.print(obj + " ");
-		}
-		System.out.println();
-	}
+//	public static void copy(List<? extends Number> source, List<? super Number> target) {
+//		for (Number n : source) {
+//			target.add(n);
+//		}
+//	}
+//
+//	public static void printList(List<?> list) {
+//		for (Object obj : list) {
+//			System.out.print(obj + " ");
+//		}
+//		System.out.println();
+//	}
 }
