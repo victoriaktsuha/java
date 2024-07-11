6fba3878030ac2462c55347f3ad7bd26489aae1b
vickats <32991496+vickats@users.noreply.github.com>
2024-07-11 16:49:47 -0300

    247. Exercício resolvido (Set)

--- java_udemy/src/application/Generics.java
+++ java_udemy/src/application/Generics.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
-import java.util.TreeSet;
 
-import entities.ProductSetGenerics;
-import entities.ProductTreeSetGenerics;
+import entities.LogEntry;
 
 public class Generics {
 
@@ -419,15 +424,52 @@ public class Generics {
 		
 		//246. Como TreeSet compara os elementos
 		
-		Set<ProductTreeSetGenerics> set = new TreeSet<>();
+//		Set<ProductTreeSetGenerics> set = new TreeSet<>();
+//		
+//		set.add(new ProductTreeSetGenerics("TV", 900.0));
+//		set.add(new ProductTreeSetGenerics("Notebook", 1200.0));
+//		set.add(new ProductTreeSetGenerics("Tablet", 400.0));
+//		
+//		for (ProductTreeSetGenerics p : set) {
+//			System.out.println(p);
+//		}
 		
-		set.add(new ProductTreeSetGenerics("TV", 900.0));
-		set.add(new ProductTreeSetGenerics("Notebook", 1200.0));
-		set.add(new ProductTreeSetGenerics("Tablet", 400.0));
 		
-		for (ProductTreeSetGenerics p : set) {
-			System.out.println(p);
+		//247. Exercício resolvido (Set) (https://github.com/acenelio/set1-java)
+		
+		/* Um site de internet registra um log de acessos dos usuários. Um registro
+		 * de log consiste no nome do usuário (apenas uma palavra) e o instante em
+		 * que o usuário acessou o site no padrão ISO 8601, separados por espaço,
+		 * conforme exemplo. Fazer um programa que leia o log de acessos a partir de
+		 * um arquivo, e daí informe quantos usuários distintos acessaram o site.
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter file full path: ");
+		String path = sc.nextLine();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			Set<LogEntry> set = new HashSet<>();
+			
+			String line = br.readLine();
+			while (line != null) {
+				
+				String[] fields = line.split(" ");
+				String username = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+				
+				set.add(new LogEntry(username, moment));
+				
+				line = br.readLine();
+			}
+			System.out.println("Total users: " + set.size());
 		}
+		catch(IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		sc.close();
 	}
 
 	/* END MAIN() */
