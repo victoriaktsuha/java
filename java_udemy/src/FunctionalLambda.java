aaebd9edae3e8408506d48bee0a99bf87ea425e7
vickats <32991496+vickats@users.noreply.github.com>
2024-07-18 15:35:47 -0300

    263.Exercicio de fixação

--- java_udemy/src/FunctionalLambda.java
+++ java_udemy/src/FunctionalLambda.java
@@ -2,12 +2,11 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
-import entities.ProductFunctionalLambda;
+import entities.EmployeeLambda;
 
 public class FunctionalLambda {
 
@@ -469,37 +468,87 @@ public class FunctionalLambda {
 		 * 
 		 */
 		
-		Scanner sc = new Scanner(System.in);
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.print("Enter full file path: ");
+//		String path = sc.nextLine();
+//		
+//		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+//			
+//			List<ProductFunctionalLambda> list = new ArrayList<>();
+//			
+//			String line = br.readLine();
+//			while(line != null){
+//				String[] fields = line.split(",");
+//				list.add(new ProductFunctionalLambda(fields[0], Double.parseDouble(fields[1])));
+//				line = br.readLine();
+//			}
+//			
+//			double avg = list.stream().map(p -> p.getPrice()).reduce(0.0 , (x, y) -> x + y) / list.size();
+//			
+//			System.out.println("Average price: " + String.format("%.2f", avg));
+//			
+//			Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+//			
+//			List<String> names = list.stream()
+//					.filter(p -> p.getPrice() < avg)
+//					.map(p -> p.getName())
+//					.sorted(comp.reversed())
+//					.collect(Collectors.toList());
+//			
+//			names.forEach(System.out::println);
+//			
+//		} catch(IOException e) {
+//			System.out.println("Error: " + e.getMessage());
+//		}
+//		sc.close();
+		
+		
+		//263.Exercicio de fixação (https://github.com/acenelio/lambda7-java)
+		
+		/* Fazer um programa para ler os dados (nome, email e salário) de funcionários a partir de 
+		 * um arquivo em formato .csv. Em seguida mostrar, em ordem alfabética, o email dos funcionários 
+		 * cujo salário seja superior a um dado valor fornecido pelo usuário. Mostrar também a soma dos 
+		 * salários dos funcionários cujo nome começa com a letra 'M'.
+		 * 
+		 */
+		
+Scanner sc = new Scanner(System.in);
 		
 		System.out.print("Enter full file path: ");
 		String path = sc.nextLine();
 		
-		try(BufferedReader br = new BufferedReader(new FileReader(path))){
-			
-			List<ProductFunctionalLambda> list = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			List<EmployeeLambda> list = new ArrayList<>();
 			
 			String line = br.readLine();
-			while(line != null){
+			while (line != null) {
 				String[] fields = line.split(",");
-				list.add(new ProductFunctionalLambda(fields[0], Double.parseDouble(fields[1])));
+				list.add(new EmployeeLambda(fields[0], fields[1], Double.parseDouble(fields[2])));
 				line = br.readLine();
 			}
+
+			System.out.print("Enter salary: ");
+			double salary = sc.nextDouble();
 			
-			double avg = list.stream().map(p -> p.getPrice()).reduce(0.0 , (x, y) -> x + y) / list.size();
-			
-			System.out.println("Average price: " + String.format("%.2f", avg));
-			
-			Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
-			
-			List<String> names = list.stream()
-					.filter(p -> p.getPrice() < avg)
-					.map(p -> p.getName())
-					.sorted(comp.reversed())
+			List<String> emails = list.stream()
+					.filter(x -> x.getSalary() > salary)
+					.map(x -> x.getEmail())
+					.sorted()
 					.collect(Collectors.toList());
+
+			System.out.println("Email of people whose salary is more than " + String.format("%.2f", salary) + ":");
+			emails.forEach(System.out::println);
 			
-			names.forEach(System.out::println);
+			double sum = list.stream()
+					.filter(x -> x.getName().charAt(0) == 'M')
+					.map(x -> x.getSalary())
+					.reduce(0.0, (x, y) -> x + y);
 			
-		} catch(IOException e) {
+			System.out.println("Sum of salary from people whose name starts with 'M': " + String.format("%.2f", sum));
+			
+		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
 		sc.close();
