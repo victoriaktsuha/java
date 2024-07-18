8ea21f6e8ab606153a5d88383633e94de5a7f528
vickats <32991496+vickats@users.noreply.github.com>
2024-07-18 14:46:44 -0300

    261. Pipeline

--- java_udemy/src/FunctionalLambda.java
+++ java_udemy/src/FunctionalLambda.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FunctionalLambda {
@@ -415,20 +416,40 @@ public class FunctionalLambda {
 		 * - Stream.iterate
 		 */
 		
+//		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+//		Stream<Integer> st1 = list.stream().map(x -> x * 10);		
+//		System.out.println(Arrays.toString(st1.toArray()));
+//		
+//		Stream<String> st2 = Stream.of("Bob", "John", "Mary");		
+//		System.out.println(Arrays.toString(st2.toArray()));
+//		
+//		//começa com 0 e o próximo serão adicionados a 2 - sequencia infinita, que será limitada pelo limit()
+//		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);		
+//		System.out.println(Arrays.toString(st3.limit(10).toArray()));
+//		
+//		//fibonacci - irá gerar uma sequencia de pares		
+//		Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p[0] + p[1]}).map(p -> p[0]);		
+//		System.out.println(Arrays.toString(st4.limit(10).toArray()));
+				
+		
+		//261. Pipeline
+		
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
-		Stream<Integer> st1 = list.stream().map(x -> x * 10);		
+		
+		//operação intermediaria (map())
+		Stream<Integer> st1 = list.stream().map(x -> x * 10);	
+		//operação terminal (toArray())
 		System.out.println(Arrays.toString(st1.toArray()));
 		
-		Stream<String> st2 = Stream.of("Bob", "John", "Mary");		
-		System.out.println(Arrays.toString(st2.toArray()));
+		//operação terminal (reduce())
+		int sum = list.stream().reduce(0, (x , y) -> x + y);
+		System.out.println("Sum = " + sum);
 		
-		//começa com 0 e o próximo serão adicionados a 2 - sequencia infinita, que será limitada pelo limit()
-		Stream<Integer> st3 = Stream.iterate(0, x -> x + 2);		
-		System.out.println(Arrays.toString(st3.limit(10).toArray()));
-		
-		//fibonacci - irá gerar uma sequencia de pares		
-		Stream<Long> st4 = Stream.iterate(new Long[] {0L, 1L}, p -> new Long[] {p[1], p[0] + p[1]}).map(p -> p[0]);		
-		System.out.println(Arrays.toString(st4.limit(10).toArray()));
+		List<Integer> newList = list.stream()
+				.filter(x -> x % 2 == 0)
+				.map(x -> x * 10)
+				.collect(Collectors.toList());
+		System.out.println(Arrays.toString(newList.toArray()));
 
 	}
 	/* END MAIN */
