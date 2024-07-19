f95a3a51b74a3ec4c9215a937b324462a0459fbc
vickats <32991496+vickats@users.noreply.github.com>
2024-07-19 09:18:32 -0300

    267. Nivelamento: Álgebra Relacional e SQL

--- java_udemy/src/application/DBJDBC.java
+++ java_udemy/src/application/DBJDBC.java
@@ -4,7 +4,10 @@ public class DBJDBC {
 
 	public static void main(String[] args) {
 		
-		/* JDBC (Java Database Connectivity): API padrão do Java
+		//266. Visão geral do JDBC
+		
+		/* 
+		 * JDBC (Java Database Connectivity): API padrão do Java
 		 * para acesso a dados - biblioteca que já vem no Java para 
 		 * acessar algum banco de dados
 		 * Packages: java.sql e javax.sql(API suplementar para servidores)
@@ -13,6 +16,55 @@ public class DBJDBC {
 		 * apara acessar o banco de dados específico
 		 */
 		
+		
+		//267. Nivelamento: Álgebra Relacional e SQL
+		
+		/* Operações básicas da álgebra relacional
+		 * - restrição
+		 * - Projeção
+		 * - Produto cartesiano
+		 * - Junção (produto cartesiano + restrição de chaves correspondentes)
+		 * 
+		 * Operação "produto cartesiano"
+		 * SELECT *
+		 * FROM PRODUCT, CATEGORY
+		 * Produto cartesiano: resultado da combinação de cada registro de uma tabela 
+		 * com cada registro de outra tabela
+		 * 
+		 * 
+		 * Operação "junção" 
+		 * SELECT *
+		 * FROM PRODUCT
+		 * WHERE
+		 * PRODUCT.CATEGORY_ID = CATEGORY.ID
+		 * É um produto cartesiano mais uma restrição de chave estrangeira com chave primaria;
+		 * Resultará em uma tabela com itens dos quais a chave estrangeira (ex.: CATEGORY_ID) é correspondente a
+		 * chave primária (ex.: ID)
+		 * 
+		 * Outra opção:
+		 * SELECT *
+		 * FROM PRODUCT
+		 * INNER JOIN CATEGORY cat
+		 * ON PRODUCT.CATEGORY_ID = cat.ID
+		 * 
+		 * 
+		 * Operação "restrição":
+		 * SELECT * 
+		 * FROM PRODUCT
+		 * INNER JOIN CATEGORY cat ON PRODUCT.CATEGORY_ID = cat.ID
+		 * WHERE CATEGORY.NAME = 'Computers'
+		 * Resultará em uma tabela de acordo com a restrição especificada, como especificado
+		 * em WHERE CATEGORY.NAME = 'Computers'
+		 * 
+		 * 
+		 * Operação "projeção":
+		 * SELECT PRODUCT.*, CATEGORY.NAME
+		 * FROM PRODUCT
+		 * INNER JOIN CATEGORY cat ON PRODUCT.CATEGORY_ID = cat.ID
+		 * WHERE CATEGORY.NAME = 'Computers'
+		 * Resultará em uma tabela de acordo com a restrição especificada, mas apenas com o
+		 * nome da categoria, como especificado em SELECT
+		 */
 
 	}
 
