bd1e2615d4e50d324f721ac1fc31a05b53603236
vickats <32991496+vickats@users.noreply.github.com>
2024-07-19 12:53:54 -0300

    Revert "holerite"
    This reverts commit f9ebf3d1bc81190c0ad3aa49a8736306eb13fab9.

--- jdbc1/src/db/DB.java
+++ jdbc1/src/db/DB.java
@@ -1,30 +1,5 @@
 package db;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.sql.Connection;
-import java.util.Properties;
-
 public class DB {
 	
-	private static Connection conn = null;
-	
-	public static Connection getConnection() {
-		if(conn == null) {
-			Properties props = loadProperties();
-			String url = props.getProperty("dburl");
-		}
-		return conn;
-	}
-	
-	private static Properties loadProperties() {
-		try(FileInputStream fs = new FileInputStream("db.properties")){
-			Properties props = new Properties();
-			props.load(fs);
-			return props;
-		}catch(IOException e){
-			throw new DbException(e.getMessage());
-		}
-	}
 }
