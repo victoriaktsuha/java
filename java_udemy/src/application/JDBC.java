bc3cfb75afe825aab045e6bfa27c8d77df6f851f
vickats <32991496+vickats@users.noreply.github.com>
2024-07-22 14:48:36 -0300

    277. PROJETO - criando projeto e repositório Git

--- java_udemy/src/application/JDBC.java
+++ java_udemy/src/application/JDBC.java
@@ -187,6 +187,39 @@ public class JDBC {
 		 */
 		
 		
+		//276. Padrão de projeto DAO (Data Access Object)
+		
+		/* Ideia geral do padrão DAO:
+		 * - Para cada entidade, haverá um objeto responsável por fazer acesso a dados relacionado a esta
+		 * entidade. Por exemplo:
+		 * -- Cliente: ClienteDao
+		 * -- Produto: ProdutoDao
+		 * -- Pedido: PedidoDao
+		 * -- Cada DAO será definido por uma interface.
+		 * - A injeção de dependência pode ser feita por meio do padrão de projeto Factory
+		 * 
+		 */
+		
+		
+		//277. PROJETO - criando projeto e repositório Git
+		
+		/* Project: http://github.com/acenelio/demo-dao-jdbc
+		 * 
+		 * Checklist:
+		 * - Github: create a new project
+		 * -- NOTE: choose .gitignore type as Java
+		 * - Eclipse: create new java project with MySQLConnector library
+		 * -- Copy db package and db.properties from: https://github.com/acenelio/jdbc5
+		 * - Create local repository and push to Github:
+		 * -- git init
+		 * -- git remote add origin https://github.com/acenelio/jdbc-dao-demo.git
+		 * -- git pull origin master
+		 * -- git add .
+		 * -- git commit -m "Project created"
+		 * -- git push -u origin master
+		 * 
+		 */
+		
 	}
 
 }
