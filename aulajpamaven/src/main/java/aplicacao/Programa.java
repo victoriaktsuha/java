f5d6bf20ca7a7cd5bac5fa7b618ed1faa3660b6b
vickats <32991496+vickats@users.noreply.github.com>
2024-07-24 14:28:33 -0300

    295. Nivelamento JPA / Hibernate - PARTE 3

--- aulajpamaven/src/main/java/aplicacao/Programa.java
+++ aulajpamaven/src/main/java/aplicacao/Programa.java
@@ -1,18 +1,49 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Bob,", "bob@mail.com");
-		Pessoa p2 = new Pessoa(2, "Mary,", "mary@mail.com");
-		Pessoa p3 = new Pessoa(3, "John,", "john@mail.com");
+		//criar itens 		
+//		Pessoa p1 = new Pessoa(null, "Bob", "bob@mail.com");
+//		Pessoa p2 = new Pessoa(null, "Mary", "mary@mail.com");
+//		Pessoa p3 = new Pessoa(null, "John", "john@mail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		//inicia transação para db
+//		em.getTransaction().begin();
+		
+		//adiciona ao db	
+//		em.persist(p1);
+//		em.persist(p2);
+//		em.persist(p3);		
+		
+		//envia transação para db
+//		em.getTransaction().commit();
+		
+		//buscar pelo id, converter em obj e instanciar obj
+		Pessoa p = em.find(Pessoa.class, 2);
+//		System.out.println(p);
+				
+		//apagar do db - o jpa só apaga instancias/obj monitorados (que acabou de inserir ou recuperada pelo '.find()'), e não destacados
+		//remove também é uma transação e precisa das linhas:
+		em.getTransaction().begin();
+		em.remove(p);		
+		em.getTransaction().commit();
+		
+		System.out.println("Done!");
+		
+		//fecha conexão
+		em.close();
+		emf.close();
 		
 		//Incluindo JPA para persistir os objetos em banco de dados (etapas no PDF)
 
